32ad12fa96c0262eb8dcf099ef747b1dd4c0dfa0
JeonDaehong <daehong770@gmail.com>
2023-11-25 14:04:51 +0900

    study java algorithm queue

--- stack_queue/Emergency_Room.java
+++ stack_queue/Emergency_Room.java
@@ -31,6 +31,10 @@ import java.util.*;
     1. 10:15 ~ 10:43
 
 
+    [ 공부해야 할 내용 ]
+    1. 필요하면 class를 만들어서 객체를 생성.
+
+
  */
 public class Emergency_Room {
     public static int solution(int n, int m, int[] arr) {
@@ -61,6 +65,39 @@ public class Emergency_Room {
 
         }
 
+        return answer;
+    }
+
+    // 더 좋은 방법
+    static class Person {
+        int id;
+        int priority;
+        public Person(int id, int priority) {
+            this.id = id;
+            this.priority = priority;
+        }
+    }
+
+    public static int solution2(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> Q=new LinkedList<>();
+        for(int i=0; i<n; i++){
+            Q.offer(new Person(i, arr[i]));
+        }
+        while(!Q.isEmpty()){
+            Person tmp = Q.poll();
+            for ( Person x : Q ) {
+                if ( x.priority > tmp.priority ) {
+                    Q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if( tmp != null ) {
+                answer ++;
+                if( tmp.id == m ) return answer;
+            }
+        }
         return answer;
     }
 
